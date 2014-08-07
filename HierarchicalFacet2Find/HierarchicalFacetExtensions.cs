fee69c7b7f2acac7c6e386ce6873ec5395cd9f0d
haugsten <hph@makingwaves.no>
2014-08-07 09:52:47 +0200

    Reverted HierarchicalFacetExtensions

--- HierarchicalFacet2Find/HierarchicalFacetExtensions.cs
+++ HierarchicalFacet2Find/HierarchicalFacetExtensions.cs
@@ -1,18 +1,14 @@
-﻿using EPiServer;
-using EPiServer.Core;
-using EPiServer.Find;
+﻿using EPiServer.Find;
 using EPiServer.Find.Api.Facets;
 using EPiServer.Find.Api.Querying;
 using EPiServer.Find.Helpers;
 using EPiServer.Find.Helpers.Reflection;
-using EPiServer.ServiceLocation;
-using HierarchicalFacet2Find;
 using HierarchicalFacet2Find.Api.Facets;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
 
-namespace Find.HierarchicalFacet2Find
+namespace HierarchicalFacet2Find
 {
     public static class HierarchicalFacetExtensions
     {
@@ -26,19 +22,15 @@ namespace Find.HierarchicalFacet2Find
             var fieldName = search.Client.Conventions.FieldNameConvention.GetFieldName(fieldSelector);
             return new Search<TSource, IQuery>(search, context =>
             {
-                var facetRequest = new ExtendedTermsFacetRequest(facetName)
-                {
-                    Field = fieldName,
-                    Order = "term",
-                    Size = 1000
-                };
+                var facetRequest = new ExtendedTermsFacetRequest(facetName);
+                facetRequest.Field = fieldName;
+                facetRequest.Order = "term";
+                facetRequest.Size = 1000;
                 context.RequestBody.Facets.Add(facetRequest);
             });
         }
 
-        public static HierarchicalFacet HierarchicalFacetFor<TResult>(
-            this IHasFacetResults<TResult> facetsResultsContainer,
-            Expression<Func<PageData, Hierarchy>> fieldSelector)
+        public static HierarchicalFacet HierarchicalFacetFor<TResult>(this IHasFacetResults<TResult> facetsResultsContainer, Expression<Func<TResult, Hierarchy>> fieldSelector)
         {
             fieldSelector.ValidateNotNullArgument("fieldSelector");
 
@@ -46,78 +38,28 @@ namespace Find.HierarchicalFacet2Find
             var facet = facetsResultsContainer.Facets[facetName] as TermsFacet;
 
             var resultFacet = new HierarchicalFacet();
-            if (facet == null) return resultFacet;
-
             foreach (var termCount in facet)
             {
                 if (!termCount.Term.Contains('/'))
                 {
                     // create top path
-                    resultFacet.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count, Name = termCount.GetTopicName() });
+                    resultFacet.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count });
                 }
                 else
                 {
                     // traversing paths
                     var sections = termCount.Term.Split('/');
-                    var hierarchyPath = resultFacet.Single(x => x.Path.Equals(sections[0]));
+                    var hierarchyPath = resultFacet.Where(x => x.Path.Equals(sections[0])).Single();
                     for (int i = 2; i < sections.Length; i++)
                     {
-                        hierarchyPath = hierarchyPath.Single(x => x.Path.Equals(string.Join("/", sections.Take(i))));
+                        hierarchyPath = hierarchyPath.Where(x => x.Path.Equals(string.Join("/", sections.Take(i)))).Single();
                     }
 
-                    hierarchyPath.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count, Name = termCount.GetTopicName() });
+                    hierarchyPath.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count });
                 }
             }
 
             return resultFacet;
-        }
-
-        public static HierarchicalFacet HierarchicalFacetFor<TResult>(this IHasFacetResults<TResult> facetsResultsContainer,
-            Expression<Func<TResult, Hierarchy>> fieldSelector)
-        {
-            fieldSelector.ValidateNotNullArgument("fieldSelector");
-
-            var facetName = fieldSelector.GetFieldPath();
-            var facet = facetsResultsContainer.Facets[facetName] as TermsFacet;
-
-            var resultFacet = new HierarchicalFacet();
-            if (facet == null) return resultFacet;
-
-            foreach (var termCount in facet)
-            {
-                if (!termCount.Term.Contains('/'))
-                {
-                    // create top path
-                    resultFacet.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count, Name = termCount.GetTopicName() });
-                }
-                else
-                {
-                    // traversing paths
-                    var sections = termCount.Term.Split('/');
-                    var hierarchyPath = resultFacet.Single(x => x.Path.Equals(sections[0]));
-                    for (int i = 2; i < sections.Length; i++)
-                    {
-                        hierarchyPath = hierarchyPath.Single(x => x.Path.Equals(string.Join("/", sections.Take(i))));
-                    }
-
-                    hierarchyPath.Add(new HierarchyPath { Path = termCount.Term, Count = termCount.Count, Name = termCount.GetTopicName() });
-                }
-            }
-
-            return resultFacet;
-        }
-
-        public static string GetTopicName(this TermCount termCount)
-        {
-            if (string.IsNullOrWhiteSpace(termCount.Term))
-                return string.Empty;
-            var term = termCount.Term.Split('/').Last();
-
-            var pageRef = new PageReference(term);
-            var pageData = ServiceLocator.Current.GetInstance<IContentLoader>().Get<PageData>(pageRef);
-            if (pageData == null) return string.Empty;
-
-            return pageData.PageName;
         }
     }
 }

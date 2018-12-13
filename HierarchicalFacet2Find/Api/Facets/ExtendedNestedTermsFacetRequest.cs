0504bfd4fd391703fcca84a453a4d8890b61ab12
jacobjones <jacob.jones@valtech.com>
2018-12-13 13:31:14 -0500

    Adds nested hierarchical facet functionality

---
+++ HierarchicalFacet2Find/Api/Facets/ExtendedNestedTermsFacetRequest.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Find.Api.Facets;
+using Newtonsoft.Json;
+
+namespace HierarchicalFacet2Find.Api.Facets
+{
+    internal class ExtendedNestedTermsFacetRequest : NestedTermsFacetRequest
+    {
+        public ExtendedNestedTermsFacetRequest(string name)
+            : base(name)
+        {
+        }
+
+        [JsonProperty("regex", NullValueHandling = NullValueHandling.Ignore)]
+        public string Regex { get; set; }
+
+        [JsonProperty("regex_flags", NullValueHandling = NullValueHandling.Ignore)]
+        public string RegexFlags { get; set; }
+
+        [JsonProperty("order", NullValueHandling = NullValueHandling.Ignore)]
+        public string Order { get; set; }
+    }
+}
--- HierarchicalFacet2Find/HierarchicalFacetExtensions.cs
+++ HierarchicalFacet2Find/HierarchicalFacetExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using EPiServer.Find;
 using EPiServer.Find.Helpers;
 using EPiServer.Find.Helpers.Reflection;
@@ -9,6 +8,7 @@ using System.Linq.Expressions;
 using EPiServer.Find.Api.Querying;
 using HierarchicalFacet2Find.Api.Facets;
 using EPiServer.Find.Api.Facets;
+using EPiServer.Find.Api.Querying.Filters;
 
 namespace HierarchicalFacet2Find
 {
@@ -62,6 +62,100 @@ namespace HierarchicalFacet2Find
             }
 
             return resultFacet;
+        }
+
+        public static ITypeSearch<TSource> HierarchicalFacetFor<TSource, TListItem>(this ITypeSearch<TSource> search, Expression<Func<TSource, IEnumerable<TListItem>>> enumerableFieldSelector, Expression<Func<TListItem, string>> itemFieldSelector, Expression<Func<TListItem, Filter>> filterExpression = null, Action<NestedTermsFacetRequest> facetRequestAction = null)
+        {
+            var filter = NestedFilter.Create(search.Client.Conventions, enumerableFieldSelector, filterExpression);
+            var action = !facetRequestAction.IsNotNull() ? x => x.FacetFilter = filter : new Action<NestedTermsFacetRequest>(x => {
+                x.FacetFilter = filter;
+                facetRequestAction(x);
+            });
+            return search.AddNestedHierarchicalFacetFor(enumerableFieldSelector, itemFieldSelector, action);
+        }
+
+        public static HierarchicalFacet HierarchicalFacetFor<TResult, TEnumerableItem>(this IHasFacetResults<TResult> facetsResultsContainer, Expression<Func<TResult, IEnumerable<TEnumerableItem>>> enumerableFieldSelector, Expression<Func<TEnumerableItem, string>> itemFieldSelector)
+        {
+            enumerableFieldSelector.ValidateNotNullArgument(nameof(enumerableFieldSelector));
+            itemFieldSelector.ValidateNotNullArgument(nameof(itemFieldSelector));
+
+            string fieldPath = string.Concat(enumerableFieldSelector.GetFieldPath(), enumerableFieldSelector.GetFieldPath().Length > 0 ? "." : "",
+                itemFieldSelector.GetFieldPath());
+
+            TermsFacet facet = facetsResultsContainer.Facets[fieldPath] as TermsFacet;
+            HierarchicalFacet hierarchicalFacet = new HierarchicalFacet();
+
+            foreach (var termCount in facet.OrderBy(x => x.Term.Count(c => c == '/')))
+            {
+                if (string.IsNullOrEmpty(termCount.Term))
+                {
+                    continue;
+                }
+
+                if (!termCount.Term.Contains('/'))
+                {
+                    hierarchicalFacet.Add(new HierarchyPath
+                    {
+                        Path = termCount.Term,
+                        Count = termCount.Count
+                    });
+
+                    continue;
+                }
+
+                string[] sections = termCount.Term.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (!sections.Any())
+                {
+                    continue;
+                }
+
+                var hierarchyPath = hierarchicalFacet.FirstOrDefault(x => x.Path.Equals(sections[0]));
+
+                if (hierarchyPath == null)
+                {
+                    continue;
+                }
+
+                for (int i = 2; i < sections.Length; ++i)
+                {
+                    hierarchyPath = hierarchyPath.Single(x => x.Path.Equals(string.Join("/", sections.Take(i))));
+                }
+
+                hierarchyPath.Add(new HierarchyPath
+                {
+                    Path = termCount.Term,
+                    Count = termCount.Count
+                });
+            }
+
+            return hierarchicalFacet;
+        }
+
+        private static ITypeSearch<TSource> AddNestedHierarchicalFacetFor<TSource>(this ITypeSearch<TSource> search, Expression enumerableFieldSelector, Expression itemFieldSelector, Action<NestedTermsFacetRequest> facetRequestAction)
+        {
+            enumerableFieldSelector.ValidateNotNullArgument(nameof(enumerableFieldSelector));
+            itemFieldSelector.ValidateNotNullArgument(nameof(itemFieldSelector));
+
+            string fieldPath = string.Concat(enumerableFieldSelector.GetFieldPath(), ".", itemFieldSelector.GetFieldPath());
+
+            return new Search<TSource, IQuery>(search, context =>
+            {
+                NestedTermsFacetRequest nestedTermsFacetRequest = new ExtendedNestedTermsFacetRequest(fieldPath)
+                {
+                    Field = string.Concat(search.Client.Conventions.FieldNameConvention.GetNestedFieldPath(enumerableFieldSelector), ".", search.Client.Conventions.FieldNameConvention.GetFieldName(itemFieldSelector)),
+                    Nested = search.Client.Conventions.FieldNameConvention.GetNestedFieldPath(enumerableFieldSelector),
+                    Order = "term",
+                    Size = 1000
+                };
+
+                if (facetRequestAction.IsNotNull())
+                {
+                    facetRequestAction(nestedTermsFacetRequest);
+                }
+
+                context.RequestBody.Facets.Add(nestedTermsFacetRequest);
+            });
         }
     }
 }
